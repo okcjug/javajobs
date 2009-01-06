cb1a37e56962966b395fdea4cb1742738735e289
Brett L. Schuchert <schuchert@objectmentor.com>
2009-01-06 10:57:08 -0600

    Added list of valid values for phone type and limit field to 6 characters.

--- grails-app/domain/Phone.groovy
+++ grails-app/domain/Phone.groovy
@@ -3,4 +3,8 @@ class Phone {
 
     String type // cell|fax|office|...
     String number
+
+    static constraints = {
+        type(inList: ['Mobile', 'Fax', 'Office'], blank:false, maxLength:6)
+    }
 }
