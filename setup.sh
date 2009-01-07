ed77212a0be3cf1ab0ebd27b628f29fc00cddaa7
Jason Lee <jasonlee@halpert.(none)>
2009-01-07 09:41:50 -0600

    Sed changes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-grails update
-grails install-plugin yui
-grails install-plugin stark-security
+#grails update
+#grails install-plugin yui
+#grails install-plugin stark-security
+
+ROLES="ROLE_SUPER_USER ROLE_RECRUITER"
+
+for ROLE in $ROLES ; do 
+	CONSTANTS="$CONSTANTS\n        static final $ROLE = '$ROLE'"
+done
+
+echo $CONSTANTS
