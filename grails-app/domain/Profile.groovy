988eb410d8057ea31243441a034f6851ff2fdf43
Ryan Hoegg <ryan@ryan-hoeggs-computer.local>
2009-01-08 22:46:37 -0600

    Profile wasn't saving in BootStrap because password was required.  It's gone now.

--- grails-app/domain/Profile.groovy
+++ grails-app/domain/Profile.groovy
@@ -2,7 +2,6 @@ class Profile {
     
     String companyName
     String userName
-    String password
     String email
 
     static hasMany = [ phones : Phone,  jobs : Job ]
@@ -10,7 +9,6 @@ class Profile {
     static constraints = {
         companyName(blank:false, maxLength:64)
         userName(blank:false, maxLength:32)
-        password(blank:false, minLength:6, maxLength:32)
         email(blank:false, minLength:7, maxLength:64)
     }
 
--- grails-app/views/recruiter/home.gsp
+++ grails-app/views/recruiter/home.gsp
@@ -1,2 +1,11 @@
-<h1>Recruiter Home!</h1>
-${profile}
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="layout" content="main" />
+        <title>Create Job</title>         
+    </head>
+    <body>
+		<h1>Recruiter Home!</h1>
+		${profile}
+	</body>
+</html>
